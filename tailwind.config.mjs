d5145f9144a7b1c633ea67866437592a5007220c
Angel Garcia <angelgarciaweb@gmail.com>
2024-01-05 16:30:18 -0400

    Add dark mode and container padding options to tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		container:{
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				sm: '2rem',
+				lg: '4rem',
+				xl: '10rem',
+				'2xl': '25rem',
+			  },
+		},
 		extend: {},
 	},
 	plugins: [],
