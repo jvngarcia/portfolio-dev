f938ad885e1f393d86bb28dad4e7a60d49ca02ad
Angel Garcia <angelgarciaweb@gmail.com>
2024-01-08 08:54:25 -0400

    Add flowbite package and plugin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: 'class',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','./node_modules/flowbite/**/*.js'],
 	theme: {
 		container:{
 			center: true,
@@ -15,5 +15,7 @@ export default {
 		},
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin')
+	],
 }
