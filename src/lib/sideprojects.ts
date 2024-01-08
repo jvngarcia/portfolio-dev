db295b7306be45fc4e614585f516b9e952543ef5
Angel Garcia <angelgarciaweb@gmail.com>
2024-01-08 09:29:37 -0400

    Refactor TopProjects component and add sideProjects data

---
+++ src/lib/sideprojects.ts
@@ -0,0 +1,28 @@
+
+interface SideProjects {
+    image: string;
+    title: string;
+    description: string;
+    link: string;
+}
+  
+
+export const sideProjects:SideProjects[] = [
+    {
+      image:
+        "https://images.pexels.com/photos/19140963/pexels-photo-19140963/free-photo-of-punto-de-referencia-lago-viaje-alemania.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      title: "Noteworthy technology acquisitions 2021",
+      description:
+        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+      link: "#",
+    },
+  
+    {
+      image:
+        "https://images.pexels.com/photos/19140963/pexels-photo-19140963/free-photo-of-punto-de-referencia-lago-viaje-alemania.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      title: "Noteworthy technology acquisitions 2021",
+      description:
+        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+      link: "#",
+    },
+  ];
