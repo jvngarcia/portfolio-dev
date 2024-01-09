dbb4dd36edf046786acf7b6e9f95dd62d6d71de7
Angel Garcia <angelgarciaweb@gmail.com>
2024-01-09 10:22:24 -0400

    Add new images and update styles

--- src/lib/sideprojects.ts
+++ src/lib/sideprojects.ts
@@ -4,6 +4,7 @@ interface SideProjects {
     title: string;
     description: string;
     link: string;
+    target?: string;
     technologies: string[];
 }
   
@@ -11,41 +12,17 @@ interface SideProjects {
 export const sideProjects:SideProjects[] = [
     {
       image:
-        "https://images.pexels.com/photos/19140963/pexels-photo-19140963/free-photo-of-punto-de-referencia-lago-viaje-alemania.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Noteworthy technology acquisitions 2021",
+        "/assets/images/portfolio/indietool.jpeg",
+      title: "Indietool.tech",
       description:
-        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
-      link: "#",
+        "A directory of tools needed for an indie hacker or indie creator.",
+      link: "https://indietool.tech",
       technologies: [
-        "React",
+        "Nextjs",
         "TypeScript",
-        "JavaScript",
-        "HTML",
-        "CSS",
-        "Node",
-        "Express",
-        "MongoDB",
-      
-      ]
-    },
-  
-    {
-      image:
-        "https://images.pexels.com/photos/19140963/pexels-photo-19140963/free-photo-of-punto-de-referencia-lago-viaje-alemania.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Noteworthy technology acquisitions 2021",
-      description:
-        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
-      link: "#",
-      technologies: [
-        "React",
-        "TypeScript",
-        "JavaScript",
-        "HTML",
-        "CSS",
-        "Node",
-        "Express",
-        "MongoDB",
-      
+        "Tailwind",
+        "TRPC",
+        "Prisma",      
       ]
     },
   ];
